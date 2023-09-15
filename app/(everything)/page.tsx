fe3a487b7a32b2a933e0bea21bad181342f28635
DanielZ1337 <danielz2nd@hotmail.com>
2023-09-16 01:02:20 +0200

    Update page.tsx

--- app/(everything)/page.tsx
+++ app/(everything)/page.tsx
@@ -1,3 +1,5 @@
+import {ArrowUpRightSquare, LinkIcon, Merge} from "lucide-react";
+
 export default function Component() {
     return (
         <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48" >
@@ -19,74 +21,23 @@ export default function Component() {
 
                                 {/* Replace with your first feature section */}
                                 <FeatureSection
-                                    iconSvg={
-                                        <svg
-                                            className=" text-secondary h-6 w-6 "
-                                            fill="none"
-                                            height="24"
-                                            stroke="currentColor"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            viewBox="0 0 24 24"
-                                            width="24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <polyline points="22 12 16 12 14 15 10 15 8 12 2 12" />
-                                            <path
-                                                d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"
-                                            />
-                                        </svg>
-                                    }
-                                    title="Smart Inbox"
-                                    description="Our Smart Inbox feature helps you manage your emails efficiently by prioritizing important emails."
+                                    iconSvg={<Merge />}
+                                    title="One Place for Your Links"
+                                    description="Gather all your profiles and links in one dashboard, from Facebook to Twitter and more."
                                 />
 
                                 {/* Replace with your second feature section */}
                                 <FeatureSection
-                                    iconSvg={
-                                        <svg
-                                            className=" text-secondary h-6 w-6 "
-                                            fill="none"
-                                            height="24"
-                                            stroke="currentColor"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            viewBox="0 0 24 24"
-                                            width="24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path d="m8 6 4-4 4 4" />
-                                            <path d="M12 2v10.3a4 4 0 0 1-1.172 2.872L4 22" />
-                                            <path d="m20 22-5-5" />
-                                        </svg>
-                                    }
-                                    title="Seamless Integration"
-                                    description="Seamless Integration allows you to connect with your favorite apps and services without leaving your inbox."
+                                    iconSvg={<ArrowUpRightSquare />}
+                                    title="Simplify Your Sharing"
+                                    description="Customize your profile with a picture, bio, and background image. Easily update to keep everyone connected."
                                 />
 
                                 {/* Replace with your third feature section */}
                                 <FeatureSection
-                                    iconSvg={
-                                        <svg
-                                            className=" text-secondary h-6 w-6 "
-                                            fill="none"
-                                            height="24"
-                                            stroke="currentColor"
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            viewBox="0 0 24 24"
-                                            width="24"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <circle cx="11" cy="11" r="8" />
-                                            <path d="m21 21-4.3-4.3" />
-                                        </svg>
-                                    }
-                                    title="Advanced Customization"
-                                    description="With Advanced Customization, you can personalize your email client to suit your preferences and work style."
+                                    iconSvg={<LinkIcon/>}
+                                    title="Effortless Link Management"
+                                    description="Log in, add your links, and Your Social Hub organizes your profiles neatly."
                                 />
                             </div>
                         </div>
@@ -101,7 +52,7 @@ export default function Component() {
 function FeatureSection({ iconSvg, title, description }:{iconSvg: React.ReactNode, title: string, description: string}) {
     return (
         <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg sm:p-6 lg:p-8 " >
-            <div className="p-3 rounded-full bg-secondary-100" >
+            <div className="p-3 rounded-full bg-secondary-100 text-secondary" >
                 {iconSvg}
             </div>
             <h2 className="text-xl font-bold text-foreground" >
