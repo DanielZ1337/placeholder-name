f56822b2269a70a6ac9fc0129592d366623be3b0
DanielZ1337 <danielz2nd@hotmail.com>
2023-09-23 14:17:16 +0200

    please fix this on vercel

--- app/(everything)/profile/page.tsx
+++ app/(everything)/profile/page.tsx
@@ -1,16 +1,11 @@
-import NewLinkInput from "@/components/new-link-input";
 import {Card} from "@nextui-org/card";
-import {SaveButton} from "@/components/form-button";
 import React from "react";
-import {addNewLink} from "@/lib/actions";
-import {Button} from "@nextui-org/react";
 import {getServerSession} from "next-auth";
 import {authOptions} from "@/lib/auth";
 import {redirect} from "next/navigation";
-import {redisClient} from "@/lib/redis";
-import {createLinkKey, Link} from "@/types/links";
 import getLinksByProfileID from "@/lib/hooks/server/getLinksByProfileID";
-import SitesForm from "@/components/sites-form";
+// import SitesForm from "@/components/sites-form";
+import {Avatar} from "@nextui-org/avatar";
 
 export default async function Page() {
     const session = await getServerSession(authOptions)
@@ -25,15 +20,15 @@ export default async function Page() {
         <div className={"flex flex-col items-center"}>
             <Card className={'flex flex-col items-center gap-2 p-4 w-full md:w-1/2 lg:w-1/3 xl:w-1/4'}>
                 <div className="flex flex-col items-center gap-2 w-full">
-                    {/*<h1>Profile</h1>
+                    <h1>Profile</h1>
                     <h2>{session.user.name}</h2>
                     <h6>{session.user.email}</h6>
-                    <Avatar src={session.user.image!} alt="Profile Image"/>*/}
-                    <div className="flex flex-col gap-2">
+                    <Avatar src={session.user.image!} alt="Profile Image"/>
+                    {/*<div className="flex flex-col gap-2">
                         <h2 className={'text-2xl font-bold text-muted'}>Title to something</h2>
                         <p className={'text-muted'}>Add a new link to your profile.</p>
                         <SitesForm currentLinks={currentLinks}/>
-                    </div>
+                    </div>*/}
                 </div>
             </Card>
         </div>
