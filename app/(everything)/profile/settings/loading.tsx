9d7be872ce0ee929ed0daede23a59232cef67123
DanielZ1337 <danielz2nd@hotmail.com>
2023-09-23 17:49:09 +0200

    loading.tsx everywhere lmaoooooooooo

---
+++ app/(everything)/profile/settings/loading.tsx
@@ -0,0 +1,7 @@
+import {Spinner} from "@nextui-org/spinner";
+
+export default function Loading() {
+    return (
+        <Spinner size={"lg"} color={"current"} className={"absolute inset-0 m-auto"}/>
+    )
+}
