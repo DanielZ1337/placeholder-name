60a240f1e95f8a60850dcd22634ee76add8d44db
DanielZ1337 <danielz2nd@hotmail.com>
2023-09-24 07:23:48 +0200

    cookie modal on mobile fixed

--- components/cookie-modal.tsx
+++ components/cookie-modal.tsx
@@ -11,6 +11,11 @@ export default function CookieModal() {
     const cookies = getCookiePermissions()
     const [preferences, setPreferences] = useState<boolean>(cookies ? cookies.preferences : DefaultCookiePermissions.preferences)
     const [analytics, setAnalytics] = useState<boolean>(cookies ? cookies.analytics : DefaultCookiePermissions.analytics)
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 640)
+
+    window.addEventListener('resize', () => {
+        setIsMobile(window.innerWidth < 640)
+    })
 
     return (
         <Modal
@@ -34,9 +39,9 @@ export default function CookieModal() {
         >
             <ModalContent>
                 {(onClose) => (
-                    <>
+                    <div className="p-4 sm:p-0">
                         <ModalHeader className="flex flex-col gap-1">
-                            <h2 className="text-2xl font-bold flex items-center gap-2"><Cookie/>Cookie Policy</h2>
+                            <h2 className="text-xl sm:text-2xl font-bold flex items-center gap-2"><Cookie/>Cookie Policy</h2>
                             <p className="text-sm text-shdcnmuted-shdcnforeground">
                                 Manage your cookie settings. You
                                 can enable or disable different types of cookies below.
@@ -45,48 +50,48 @@ export default function CookieModal() {
                         <ModalBody>
                             <div className="flex flex-col gap-4">
                                 <div className="flex flex-col gap-2">
-                                    <h3 className="text-lg font-bold">Necessary Cookies</h3>
+                                    <h3 className="text-base sm:text-lg font-bold">Necessary Cookies</h3>
                                     <div className="flex gap-2">
-                                        <p className="text-shdcnmuted-shdcnforeground text-sm">
+                                        <p className="text-shdcnmuted-shdcnforeground text-xs sm:text-sm">
                                             Necessary cookies help make a website usable by enabling basic
                                             functions like page navigation and access to secure areas of the
                                             website. The website cannot function properly without these cookies.
                                         </p>
-                                        <Switch size={"lg"} color={"secondary"} isReadOnly defaultSelected/>
+                                        <Switch size={isMobile ? "md" : "lg"} color={"secondary"} isReadOnly defaultSelected/>
                                     </div>
                                 </div>
                                 <div className="flex flex-col gap-2">
-                                    <h3 className="text-lg font-bold">Preferences</h3>
+                                    <h3 className="text-base sm:text-lg font-bold">Preferences</h3>
                                     <div className="flex gap-2">
-                                        <p className="text-shdcnmuted-shdcnforeground text-sm">
+                                        <p className="text-shdcnmuted-shdcnforeground text-xs sm:text-sm">
                                             Preference cookies enable a website to remember information that
                                             changes the way the website behaves or looks, like your preferred
                                             language or the region that you are in.
                                         </p>
-                                        <Switch size={"lg"} color={"secondary"} onValueChange={setPreferences}
+                                        <Switch size={isMobile ? "md" : "lg"} color={"secondary"} onValueChange={setPreferences}
                                                 defaultSelected={preferences}/>
                                     </div>
                                 </div>
                                 <div className="flex flex-col gap-2">
-                                    <h3 className="text-lg font-bold">Analytics</h3>
+                                    <h3 className="text-base sm:text-lg font-bold">Analytics</h3>
                                     <div className="flex gap-2">
-                                        <p className="text-shdcnmuted-shdcnforeground text-sm">
+                                        <p className="text-shdcnmuted-shdcnforeground text-xs sm:text-sm">
                                             Analytics cookies help website owners to understand how visitors
                                             interact with websites by collecting and reporting information
                                             anonymously.
                                         </p>
-                                        <Switch size={"lg"} color={"secondary"} onValueChange={setAnalytics}
+                                        <Switch size={isMobile ? "md" : "lg"} color={"secondary"} onValueChange={setAnalytics}
                                                 defaultSelected={analytics}/>
                                     </div>
                                 </div>
                             </div>
                         </ModalBody>
                         <ModalFooter>
-                            <Button color="secondary" variant="ghost" onPress={onClose}>
+                            <Button size={isMobile ? "sm" : "md"} color="secondary" variant="ghost" onPress={onClose}>
                                 Save Preferences
                             </Button>
                         </ModalFooter>
-                    </>
+                    </div>
                 )}
             </ModalContent>
         </Modal>
